acfe01a1d4732ba6e5a6f80ddb80ff9bee04921c
Andrew Bradley <cspotcode@gmail.com>
2019-05-23 00:17:31 -0400

    fix

--- build.bat
+++ build.bat
@@ -20,7 +20,7 @@ REM ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
 mkdir build
 cd build
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
-cmake -G "Visual Studio 14 2015 Win64" -DCMAKE_BUILD_TYPE=Debug ..
-msbuild synergy-core.sln /p:Platform="x64" /p:Configuration=Debug /m
+cmake -G "Visual Studio 14 2015 Win64" -DCMAKE_BUILD_TYPE=Release ..
+msbuild synergy-core.sln /p:Platform="x64" /p:Configuration=Release /m
 cd ..
 copy ext\openssl\windows\x64\bin\* build\
